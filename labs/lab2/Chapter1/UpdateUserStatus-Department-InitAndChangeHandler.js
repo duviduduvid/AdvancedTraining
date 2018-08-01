c48996e8e2e381b01209c3931b86e4c397c9dc12
David <david.franco@ca.com>
2018-08-01 16:19:46 +0300

    adding comment

--- labs/lab2/Chapter1/UpdateUserStatus-Department-InitAndChangeHandler.js
+++ labs/lab2/Chapter1/UpdateUserStatus-Department-InitAndChangeHandler.js
@@ -1,3 +1,8 @@
+
+/* Update User Status form
+Department property - Initialization and Change handlers
+*/
+
 return api.server(['getTitlesByDepartment', prop.value]).then(function(result) {
     var titles = JSON.parse(result.returnValue);
     api.getProp("title").options = titles;    
